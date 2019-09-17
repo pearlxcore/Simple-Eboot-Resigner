6e1950285ff89ecbf62d3230a940efd44f0552fc
pearlxcore <36906814+pearlxcore@users.noreply.github.com>
2019-09-17 08:58:30 +0800

    add drag n drop folder feature

--- PS3 HEN EBOOT Resigner (3.55)/Form1.Designer.cs
+++ PS3 HEN EBOOT Resigner (3.55)/Form1.Designer.cs
@@ -42,72 +42,80 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtGame
             // 
+            this.txtGame.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.txtGame.BackColor = System.Drawing.Color.Gray;
             this.txtGame.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtGame.Cursor = System.Windows.Forms.Cursors.Default;
             this.txtGame.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtGame.ForeColor = System.Drawing.Color.GreenYellow;
-            this.txtGame.Location = new System.Drawing.Point(20, 155);
+            this.txtGame.Location = new System.Drawing.Point(24, 155);
             this.txtGame.Name = "txtGame";
             this.txtGame.ReadOnly = true;
-            this.txtGame.Size = new System.Drawing.Size(445, 22);
+            this.txtGame.Size = new System.Drawing.Size(567, 22);
             this.txtGame.TabIndex = 0;
             this.txtGame.TabStop = false;
             this.txtGame.MouseDown += new System.Windows.Forms.MouseEventHandler(this.txtGame_MouseDown);
             // 
             // richTextBox1
             // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.richTextBox1.BackColor = System.Drawing.Color.Gray;
             this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox1.Cursor = System.Windows.Forms.Cursors.Default;
             this.richTextBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox1.ForeColor = System.Drawing.Color.GreenYellow;
-            this.richTextBox1.Location = new System.Drawing.Point(20, 210);
+            this.richTextBox1.Location = new System.Drawing.Point(24, 210);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(445, 113);
+            this.richTextBox1.Size = new System.Drawing.Size(567, 113);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.TabStop = false;
             this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
             this.richTextBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.richTextBox1_MouseDown);
             // 
             // panel2
             // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.panel2.Controls.Add(this.label2);
             this.panel2.Controls.Add(this.label1);
-            this.panel2.Location = new System.Drawing.Point(32, 5);
+            this.panel2.Location = new System.Drawing.Point(24, 5);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(418, 129);
+            this.panel2.Size = new System.Drawing.Size(567, 129);
             this.panel2.TabIndex = 2;
             this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
             // 
             // label2
             // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonShadow;
-            this.label2.Location = new System.Drawing.Point(6, 79);
+            this.label2.Location = new System.Drawing.Point(81, 79);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(407, 34);
             this.label2.TabIndex = 1;
             this.label2.Text = "Drag && Drop EBOOT.BIN of game backups into this program. It \r\nwill create new EB" +
     "OOT.BIN and backup the original as EBOOT.BAK";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.GreenYellow;
-            this.label1.Location = new System.Drawing.Point(88, 12);
+            this.label1.Location = new System.Drawing.Point(163, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(243, 60);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Simple EBOOT Resigner\r\n     [HEN Compatible]";
+            this.label1.Text = "Simple EBOOT Resigner\r\n [PS3HEN Compatible]";
             this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
             // 
             // label3
@@ -115,7 +123,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonShadow;
-            this.label3.Location = new System.Drawing.Point(367, 327);
+            this.label3.Location = new System.Drawing.Point(453, 326);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(51, 15);
             this.label3.TabIndex = 2;
@@ -126,7 +134,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.GreenYellow;
-            this.label4.Location = new System.Drawing.Point(18, 328);
+            this.label4.Location = new System.Drawing.Point(24, 328);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(217, 13);
             this.label4.TabIndex = 3;
@@ -136,12 +144,13 @@
             // 
             this.backgroundWorker1.WorkerSupportsCancellation = true;
             this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorker1_RunWorkerCompleted);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(424, 327);
+            this.label5.Location = new System.Drawing.Point(510, 326);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(0, 15);
             this.label5.TabIndex = 4;
@@ -151,7 +160,7 @@
             this.checkBox1.AutoSize = true;
             this.checkBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.checkBox1.ForeColor = System.Drawing.SystemColors.ButtonShadow;
-            this.checkBox1.Location = new System.Drawing.Point(330, 185);
+            this.checkBox1.Location = new System.Drawing.Point(460, 185);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(135, 17);
             this.checkBox1.TabIndex = 5;
@@ -167,7 +176,7 @@
             this.comboBox1.Items.AddRange(new object[] {
             "3.41",
             "3.55"});
-            this.comboBox1.Location = new System.Drawing.Point(107, 183);
+            this.comboBox1.Location = new System.Drawing.Point(111, 183);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(75, 21);
             this.comboBox1.TabIndex = 6;
@@ -177,7 +186,7 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.ButtonShadow;
-            this.label6.Location = new System.Drawing.Point(17, 139);
+            this.label6.Location = new System.Drawing.Point(21, 139);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(36, 13);
             this.label6.TabIndex = 2;
@@ -188,18 +197,25 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.SystemColors.ButtonShadow;
-            this.label7.Location = new System.Drawing.Point(18, 187);
+            this.label7.Location = new System.Drawing.Point(21, 186);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(83, 13);
             this.label7.TabIndex = 7;
             this.label7.Text = "Select version :";
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.WorkerReportsProgress = true;
+            this.backgroundWorker2.WorkerSupportsCancellation = true;
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker2_DoWork);
+            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorker2_RunWorkerCompleted);
             // 
             // Form1
             // 
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(482, 348);
+            this.ClientSize = new System.Drawing.Size(617, 348);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.comboBox1);
@@ -245,6 +261,7 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
     }
 }
 
--- PS3 HEN EBOOT Resigner (3.55)/Form1.cs
+++ PS3 HEN EBOOT Resigner (3.55)/Form1.cs
@@ -11,14 +11,21 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Compression;
 using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace PS3_HEN_EBOOT_Resigner__3._55_
 {
     public partial class Form1 : MetroFramework.Forms.MetroForm
     {
+        List<string> allFiles = new List<string>();
         string[] files;
         string directory, elfPath, text, version;
         string newEbootPath;
+        private string[] root;
+        private int s;
+        private string[] folders;
+        private string elfPath_bulk;
+        private string newEbootPath_bulk;
         public const int WM_NCLBUTTONDOWN = 0xA1;
         public const int HTCAPTION = 0x2;
 
@@ -70,6 +77,11 @@ namespace PS3_HEN_EBOOT_Resigner__3._55_
                 File.Delete("scetool.exe");
             }
 
+        }
+
+        private void processBulk()
+        {
+            
         }
 
         private void process()
@@ -176,11 +188,14 @@ namespace PS3_HEN_EBOOT_Resigner__3._55_
                 else
                 {
                     label5.ForeColor = Color.Red;
-                    label5.Text = "FAIL!";
+                    label5.Text = "FAIL";
                 }
             }
             else
             {
+                richTextBox1.Text += "[*] Not a valid EBOOT.BIN file!";
+                label5.ForeColor = Color.Red;
+                label5.Text = "FAIL";
 
             }
         }
@@ -221,6 +236,168 @@ namespace PS3_HEN_EBOOT_Resigner__3._55_
             }
         }
 
+        private void BackgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            foreach (string folder in folders)
+            {
+                try
+                {
+                    allFiles.AddRange(Directory.GetFiles(folder, "EBOOT.BIN", SearchOption.AllDirectories));
+                    richTextBox1.Text += "[*] Searching folder : " + folder + "\n";
+                }
+                catch { } // Don't know what the problem is, don't care...
+
+                
+            }
+
+            allFiles.ToArray();
+
+            foreach (var fullpath in allFiles)
+            {
+                elfPath_bulk = Path.GetDirectoryName(fullpath) + "\\EBOOT.ELF";
+                newEbootPath_bulk = Path.GetDirectoryName(fullpath) + "\\EBOOT355.BIN";
+
+                richTextBox1.Text += "--------------------------------------------------\n[*] Reading EBOOT.BIN Info : " + fullpath + "\n";
+
+                Process scetool = new Process();
+                scetool.StartInfo.FileName = "scetool.exe";
+                scetool.StartInfo.Arguments = " -i \"" + fullpath + "\"";
+                scetool.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                scetool.StartInfo.UseShellExecute = false;
+                scetool.StartInfo.RedirectStandardOutput = true;
+                scetool.StartInfo.CreateNoWindow = true;
+                scetool.Start();
+
+                text += scetool.StandardOutput.ReadToEnd();
+
+                if (text.Contains("SELF-Type [Application]"))
+                {
+                    richTextBox1.Text += "[*] Decrypting EBOOT.BIN to EBOOT.ELF..\n";
+
+                    Process decrypt = new Process();
+                    decrypt.StartInfo.FileName = "scetool.exe";
+                    decrypt.StartInfo.Arguments = " -d \"" + fullpath + "\" \"" + elfPath_bulk + "\"";
+                    decrypt.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    decrypt.StartInfo.UseShellExecute = false;
+                    decrypt.StartInfo.RedirectStandardOutput = true;
+                    decrypt.StartInfo.CreateNoWindow = true;
+                    decrypt.Start();
+                    text = decrypt.StandardOutput.ReadToEnd();
+
+                    if (File.Exists(elfPath_bulk))
+                    {
+                        richTextBox1.Text += "[*] Backing up original EBOOT.BIN.." + "\n";
+
+                        string path = Path.GetDirectoryName(fullpath);
+                        try
+                        {
+                            System.IO.File.Move(fullpath, path + "\\EBOOT.BAK");
+
+                        }
+                        catch
+                        {
+
+                        }
+
+
+                        if (comboBox1.SelectedItem == "3.41")
+                        {
+                            version = "0003004100000000";
+                        }
+                        else if (comboBox1.SelectedItem == "3.55")
+                        {
+                            version = "0003005500000000";
+                        }
+
+
+
+                        Process encrypt = new Process();
+                        encrypt.StartInfo.FileName = "scetool.exe";
+                        if (checkBox1.Checked)
+                        {
+                            encrypt.StartInfo.Arguments = " --verbose --sce-type=SELF --skip-sections=FALSE --compress-data=TRUE --self-add-shdrs=TRUE --key-revision=0A --self-auth-id=1010000001000003 --self-vendor-id=01000002 --self-ctrl-flags=0000000000000000000000000000000000000000000000000000000000000000 --self-cap-flags=00000000000000000000000000000000000000000000003B0000000100040000 --self-app-version=0001000000000000 --self-type=APP --self-fw-version=" + version + " --encrypt " + "\"" + elfPath_bulk + "\" " + "\"" + path + "\\EBOOT.BIN" + "\"";
+                            richTextBox1.Text += "[*] Encrypting and compressing EBOOT.BIN..\n";
+
+                        }
+                        else
+                        {
+                            encrypt.StartInfo.Arguments = " --verbose --sce-type=SELF --skip-sections=FALSE --compress-data=FALSE --self-add-shdrs=TRUE --key-revision=0A --self-auth-id=1010000001000003 --self-vendor-id=01000002 --self-ctrl-flags=0000000000000000000000000000000000000000000000000000000000000000 --self-cap-flags=00000000000000000000000000000000000000000000003B0000000100040000 --self-app-version=0001000000000000 --self-type=APP --self-fw-version=" + version + " --encrypt " + "\"" + elfPath_bulk + "\" " + "\"" + path + "\\EBOOT.BIN" + "\"";
+                            richTextBox1.Text += "[*] Encrypting EBOOT.BIN..\n";
+                        }
+                        encrypt.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                        encrypt.StartInfo.UseShellExecute = false;
+                        encrypt.StartInfo.RedirectStandardOutput = true;
+                        encrypt.StartInfo.CreateNoWindow = true;
+                        encrypt.Start();
+                        text = encrypt.StandardOutput.ReadToEnd();
+
+                        //richTextBox1.Text += "Args = " + encrypt.StartInfo.FileName + encrypt.StartInfo.Arguments + "\n\n";
+                        //richTextBox1.Text += text;
+
+                        richTextBox1.Text += "[*] Deleting files..\n";
+
+                        if (File.Exists(elfPath_bulk))
+                        {
+                            File.Delete(elfPath_bulk);
+                        }
+                        richTextBox1.Text += "[*] EBOOT.BIN resigned to " + comboBox1.Text + "\n";
+                        System.Threading.Thread.Sleep(500);
+                        s++;
+
+                    }
+                }
+                else
+                {
+                    richTextBox1.Text += "[*] Not a valid EBOOT.BIN file!";
+                    label5.ForeColor = Color.Red;
+                    label5.Text = "FAIL";
+
+                }
+
+            }
+
+            if(s != 0)
+            {
+                richTextBox1.Text += "[*] Done!";
+                //MessageBox.Show("Total EBOOT.BIN found : " + s.ToString(), "PS3 EBOOT RESIGNER");
+                label5.ForeColor = Color.YellowGreen;
+                label5.Text = "DONE";
+            }
+            else
+            {
+                label5.ForeColor = Color.Red;
+                label5.Text = "FAIL";
+                richTextBox1.Text += "[*] EBOOT.BIN not found!";
+            }
+
+
+
+        }
+
+        private void BackgroundWorker2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Array.Clear(root, 0, root.Length);
+            Array.Clear(folders, 0, folders.Length);
+            allFiles.Clear();
+            s = 0;
+            checkBox1.Enabled = true;
+            comboBox1.Enabled = true;
+        }
+
+        private void BackgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            checkBox1.Enabled = true;
+            comboBox1.Enabled = true;
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            // set the current caret position to the end
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+            // scroll it automatically
+            richTextBox1.ScrollToCaret();
+        }
+
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
             try
@@ -251,7 +428,6 @@ namespace PS3_HEN_EBOOT_Resigner__3._55_
 
         private void Form1_DragDrop(object sender, DragEventArgs e)
         {
-            label5.ForeColor = Color.Black;
             label5.Text = "";
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
@@ -259,17 +435,35 @@ namespace PS3_HEN_EBOOT_Resigner__3._55_
                 if (files != null && files.Length != 0)
                 {
 
-                   
                     richTextBox1.Clear();
                     txtGame.Text = files[0];
                     directory = txtGame.Text;
-                    elfPath = Path.GetDirectoryName(txtGame.Text) + "\\EBOOT.ELF";
-                    newEbootPath = Path.GetDirectoryName(txtGame.Text) + "\\EBOOT355.BIN";
+                    checkBox1.Enabled = false;
+                    comboBox1.Enabled = false;
+
+                    if (Directory.Exists(txtGame.Text))
+                    {
+                        
+                        root = Directory.GetFiles(txtGame.Text, "EBOOT.BIN");
+                        allFiles.AddRange(root);
+                        folders = Directory.GetDirectories(txtGame.Text);
+
+                        if (backgroundWorker2.IsBusy)
+                            backgroundWorker2.CancelAsync();
+                        backgroundWorker2.RunWorkerAsync();
+                    }
+                    else
+                    {
+                        directory = txtGame.Text;
+                        elfPath = Path.GetDirectoryName(txtGame.Text) + "\\EBOOT.ELF";
+                        newEbootPath = Path.GetDirectoryName(txtGame.Text) + "\\EBOOT355.BIN";
 
 
-                    if (backgroundWorker1.IsBusy)
-                        backgroundWorker1.CancelAsync();
-                    backgroundWorker1.RunWorkerAsync();
+                        if (backgroundWorker1.IsBusy)
+                            backgroundWorker1.CancelAsync();
+                        backgroundWorker1.RunWorkerAsync();
+                    }
+                    
 
 
                 }
